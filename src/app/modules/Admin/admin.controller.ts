82601b3c83449b17189127d412851bf6213f2680
Naimur Rahman <naimur2231@gmail.com>
2024-03-30 00:56:56 +0600

    Deleting Data From The Database

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -61,8 +61,28 @@ const updateIntoDB = async (req: Request, res: Response) => {
   }
 };
 
+const deleteFromDB = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const result = await AdminService.deleteFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Admin data deleted!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrond",
+      error: err,
+    });
+  }
+};
+
 export const AdminController = {
   getAllFromDB,
   getByIdFromDB,
   updateIntoDB,
+  deleteFromDB,
 };
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -4,7 +4,11 @@ import { AdminController } from "./admin.controller";
 const router = express.Router();
 
 router.get("/", AdminController.getAllFromDB);
+
 router.get("/:id", AdminController.getByIdFromDB);
+
 router.patch("/:id", AdminController.updateIntoDB);
+
+router.delete("/:id", AdminController.deleteFromDB);
 
 export const AdminRoutes = router;
