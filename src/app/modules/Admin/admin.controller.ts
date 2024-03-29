f2d956f80d7b972191e7a33bda4073e440189949
Naimur Rahman <naimur2231@gmail.com>
2024-03-30 00:33:07 +0600

    Updating Data In The Database By ID

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -42,7 +42,27 @@ const getByIdFromDB = async (req: Request, res: Response) => {
   }
 };
 
+const updateIntoDB = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const result = await AdminService.updateIntoDB(id, req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin data updated!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrond",
+      error: err,
+    });
+  }
+};
+
 export const AdminController = {
   getAllFromDB,
   getByIdFromDB,
+  updateIntoDB,
 };
