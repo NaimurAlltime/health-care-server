f5bd2d8ed0669cbf06e9d7136f96c88dd5dce178
Naimur Rahman <naimur2231@gmail.com>
2024-03-29 15:58:32 +0600

    Optimization Of Search Code

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,25 +1,42 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
+  const andCondition: Prisma.AdminWhereInput[] = [];
+
+  // OR: [
+  //   {
+  //     name: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  //   {
+  //     email: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  // ],
+
+  const adminSearchAbleField = ["name", "email"];
+
+  if (params.searchTerm) {
+    andCondition.push({
+      OR: adminSearchAbleField.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
+
   const result = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where: whereCondition,
   });
   return result;
 };
