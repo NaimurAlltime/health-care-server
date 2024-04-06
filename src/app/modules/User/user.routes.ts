402ee5d4f09dbb2add85e55b1c9c074680d42dbd
Naimur Rahman <naimur2231@gmail.com>
2024-04-06 11:46:25 -0700

    Implementing Authentication Middleware For Role-Based Access On APIs

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
-import { userValidation } from "./user.validation";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post(
-    "/create-admin", userController.createAdmin);
+    "/create-admin", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), userController.createAdmin);
 
 export const userRoutes = router;
