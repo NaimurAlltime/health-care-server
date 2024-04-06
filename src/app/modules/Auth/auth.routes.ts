924205df9bda89b6b8efe232fd6782fcda4d643c
Naimur Rahman <naimur2231@gmail.com>
2024-04-06 12:27:32 -0700

    Implementing Changing Password:

--- src/app/modules/Auth/auth.routes.ts
+++ src/app/modules/Auth/auth.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
@@ -13,6 +15,16 @@ router.post(
     AuthController.refreshToken
 )
 
+router.post(
+    '/change-password',
+    auth(
+        UserRole.SUPER_ADMIN,
+        UserRole.ADMIN,
+        UserRole.DOCTOR,
+        UserRole.PATIENT
+    ),
+    AuthController.changePassword
+);
 
 
 export const AuthRoutes = router;
