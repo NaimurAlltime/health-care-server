6444986253c4aa8808bb1b1b0878b73e54bff8cb
Naimur Rahman <naimur2231@gmail.com>
2024-04-08 11:27:32 -0700

    docter validation schema and routes created

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -18,6 +18,41 @@ const createAdmin = z.object({
     })
 });
 
+const createDoctor = z.object({
+    password: z.string({
+        required_error: "Password is required"
+    }),
+    doctor: z.object({
+        name: z.string({
+            required_error: "Name is required!"
+        }),
+        email: z.string({
+            required_error: "Email is required!"
+        }),
+        contactNumber: z.string({
+            required_error: "Contact Number is required!"
+        }),
+        address: z.string().optional(),
+        registrationNumber: z.string({
+            required_error: "Reg number is required"
+        }),
+        experience: z.number().optional(),
+        gender: z.enum([Gender.MALE, Gender.FEMALE]),
+        appointmentFee: z.number({
+            required_error: "appointment fee is required"
+        }),
+        qualification: z.string({
+            required_error: "quilification is required"
+        }),
+        currentWorkingPlace: z.string({
+            required_error: "Current working place is required!"
+        }),
+        designation: z.string({
+            required_error: "Designation is required!"
+        })
+    })
+});
+
 
 
 const updateStatus = z.object({
@@ -28,5 +63,6 @@ const updateStatus = z.object({
 
 export const userValidation = {
     createAdmin,
-    updateStatus
+    updateStatus,
+    createDoctor
 }
